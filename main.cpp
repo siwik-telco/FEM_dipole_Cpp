b082913eccddf9175d5776a7f442fcf6482da05c
barteksiwik <bartolopoland@gmail.com>
2025-10-17 14:46:00 +0200

    Work is done but i dont think it works XD

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "dipoleAntenna.h"
 #include "ImpedanceMatcher.h"
 int main () {
-    double f_min, f_max, length, width, R_load;
+    double f_min, f_max, length, width, R_load1;
     int n_points;
 
     std::cout << "Min freq: \n";
@@ -18,14 +18,31 @@ int main () {
     std::cin >> width;
 
     std::cout << "Imput impendance of system: \n";
-    std::cin >> R_load;
+    std::cin >> R_load1;
 
     std::cout << "Number of points in simulation: \n";
     std::cin >> n_points;
 
-    dipoleAntenna(length, width);
-    ImpedanceMatcher(R_load);
+    dipoleAntenna dipole(length, width);
+    ImpedanceMatcher matcher(R_load1);
 
+    double df =  (f_max-f_min) / (n_points - 1.0);
+    std::cout << "test";
 
+    double minRL = -50;
+    double fbest = 0.0;
+    std::complex<double> Z_best;
 
+    for (int i=0; i<n_points;i++) {
+        double f = f_min + i * df;
+        double freq = f/1e6;
+        std::complex<double> Z =dipole.inputZ(f);
+        double R = std::real(Z);
+        double X = std::imag(Z);
+        double L = dipole.inductance(f);
+        double gam = matcher.RL(Z);
+        std::cout << "RL = " << gam << ".\n";
+        // std::cout << "R: X: L: " << R << " " << X << " " << L << ".\n";
+    }
+    return 0;
 }
